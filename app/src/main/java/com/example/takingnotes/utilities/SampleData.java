8345a99456d5adf12b90442688630b3e285526ea
Derek Altman <derek_a_altman@yahoo.com>
2020-02-11 17:39:20 -0600

    Create SampleData.java

---
+++ app/src/main/java/com/example/takingnotes/utilities/SampleData.java
@@ -0,0 +1,35 @@
+package com.example.takingnotes.utilities;
+
+import com.example.takingnotes.database.NoteEntity;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+public class SampleData {
+
+    private static final String SAMPLE_TEXT_1 = "A simple note";
+    private static final String SAMPLE_TEXT_2 = "A note with\nline feed";
+    private static final String SAMPLE_TEXT_3 = "Lorem ipsum dolor sit amet, consectetur adipiscing " +
+            "elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
+            "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\n " +
+            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat " +
+            "nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui " +
+            "officia deserunt mollit anim id est laborum.";
+
+    private static Date getDate(int diff) {
+        GregorianCalendar cal = new GregorianCalendar();
+        cal.add(Calendar.MILLISECOND, diff);
+        return cal.getTime();
+    }
+
+    public static List<NoteEntity> getNotes() {
+        List<NoteEntity> notes = new ArrayList<>();
+        notes.add(new NoteEntity(getDate(0), SAMPLE_TEXT_1));
+        notes.add(new NoteEntity(getDate(-1), SAMPLE_TEXT_2));
+        notes.add(new NoteEntity(getDate(-2), SAMPLE_TEXT_3));
+        return notes;
+    }
+}
