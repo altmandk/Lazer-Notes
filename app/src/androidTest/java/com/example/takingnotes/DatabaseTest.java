ae66728a908d5be090620cb69c24597b164428e0
Derek Altman <derek_a_altman@yahoo.com>
2020-02-11 17:32:42 -0600

    Create DatabaseTest.java

---
+++ app/src/androidTest/java/com/example/takingnotes/DatabaseTest.java
@@ -0,0 +1,59 @@
+package com.example.takingnotes;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.room.Room;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.takingnotes.database.AppDatabase;
+import com.example.takingnotes.database.NoteDao;
+import com.example.takingnotes.database.NoteEntity;
+import com.example.takingnotes.utilities.SampleData;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class DatabaseTest {
+    public static final String TAG = "Junit";
+    private AppDatabase mDb;
+    private NoteDao mDao;
+
+    @Before
+    public void createDb() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        mDb = Room.inMemoryDatabaseBuilder(context,
+                AppDatabase.class).build();
+        mDao = mDb.noteDao();
+        Log.i(TAG, "createDb");
+    }
+
+    @After
+    public void closeDb() {
+        mDb.close();
+        Log.i(TAG, "closeDb");
+    }
+
+    @Test
+    public void createAndRetrieveNotes() {
+        mDao.insertAll((SampleData.getNotes()));
+        int count = mDao.getCount();
+        Log.i(TAG, "createAndRetrieveNotes: count=" + count);
+        assertEquals(SampleData.getNotes().size(), count);
+    }
+
+    @Test
+    public void compareStrings(){
+        mDao.insertAll(SampleData.getNotes());
+        NoteEntity original = SampleData.getNotes().get(0);
+        NoteEntity fromDb = mDao.getNoteByID(1);
+        assertEquals(original.getText(), fromDb.getText());
+        assertEquals(1, fromDb.getId());
+    }
+}
